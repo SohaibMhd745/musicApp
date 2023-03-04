0b41efa322dc181be3a9d4aad3e203a3ce80ff24
PHANGWilly <pro.phang.willy@gmail.com>
2023-03-04 22:36:23 +0100

    Songs appear now

--- app/js/player.js
+++ app/js/player.js
@@ -6,3 +6,10 @@ switch (player){
     default :
         console.log("playing");
 }
+
+const loop = document.querySelectorAll("loop")
+for (let i=0; i<loop.length; i++) {
+    console.log("music appears");
+    console.log("music is playing");
+    console.log("loop is playing");
+}
